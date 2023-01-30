1794616edf12bd6028a426dfcac3263ae69d39ae
Janne Valkealahti <janne.valkealahti@gmail.com>
2023-01-30 16:08:04 +0000

    Add missing native hints for initializr

--- src/main/java/org/springframework/cli/config/SpringCliRuntimeHints.java
+++ src/main/java/org/springframework/cli/config/SpringCliRuntimeHints.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 the original author or authors.
+ * Copyright 2021-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 package org.springframework.cli.config;
 
 import java.util.List;
@@ -26,6 +24,26 @@ import org.springframework.aot.hint.ReflectionHints;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.aot.hint.TypeReference;
+import org.springframework.cli.initializr.model.ArtifactId;
+import org.springframework.cli.initializr.model.BootVersion;
+import org.springframework.cli.initializr.model.Dependencies;
+import org.springframework.cli.initializr.model.Dependency;
+import org.springframework.cli.initializr.model.DependencyCategory;
+import org.springframework.cli.initializr.model.Description;
+import org.springframework.cli.initializr.model.GroupId;
+import org.springframework.cli.initializr.model.IdName;
+import org.springframework.cli.initializr.model.JavaVersion;
+import org.springframework.cli.initializr.model.JavaVersion.JavaVersionValues;
+import org.springframework.cli.initializr.model.Language;
+import org.springframework.cli.initializr.model.Language.LanguageValues;
+import org.springframework.cli.initializr.model.Metadata;
+import org.springframework.cli.initializr.model.Name;
+import org.springframework.cli.initializr.model.PackageName;
+import org.springframework.cli.initializr.model.Packaging;
+import org.springframework.cli.initializr.model.Packaging.PackagingValues;
+import org.springframework.cli.initializr.model.ProjectType;
+import org.springframework.cli.initializr.model.ProjectType.ProjectTypeValue;
+import org.springframework.cli.initializr.model.Version;
 
 public class SpringCliRuntimeHints implements RuntimeHintsRegistrar {
 
@@ -44,6 +62,23 @@ public class SpringCliRuntimeHints implements RuntimeHintsRegistrar {
 				"org.kohsuke.github.GHVerification",
 				"org.kohsuke.github.GitUser"
 		);
+		registerForMostReflection(hints.reflection(), ArtifactId.class, BootVersion.class, Dependencies.class,
+				Dependency.class, DependencyCategory.class, Description.class, GroupId.class, IdName.class,
+				JavaVersion.class, JavaVersionValues.class, Language.class, LanguageValues.class, Metadata.class,
+				Name.class, PackageName.class, Packaging.class, PackagingValues.class, ProjectType.class,
+				ProjectTypeValue.class, Version.class);
+	}
+
+	private void registerForMostReflection(ReflectionHints reflectionHints, Class<?>... classes) {
+		for (Class<?> clazz : classes) {
+			reflectionHints.registerType(clazz, hint -> {
+				hint.withMembers(
+					MemberCategory.DECLARED_CLASSES, MemberCategory.DECLARED_FIELDS,
+					MemberCategory.PUBLIC_CLASSES, MemberCategory.PUBLIC_FIELDS,
+					MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_METHODS,
+					MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INTROSPECT_DECLARED_METHODS);
+			});
+		}
 	}
 
 	private void registerForMostReflection(ReflectionHints reflectionHints, String... classNames) {
