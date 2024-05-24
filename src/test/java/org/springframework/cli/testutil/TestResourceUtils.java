dd2a873d293b5c2f8f43bd9a80c52829f0d1081a
Janne Valkealahti <janne.valkealahti@gmail.com>
2024-05-24 12:09:01 +0100

    Polish

--- src/test/java/org/springframework/cli/testutil/TestResourceUtils.java
+++ src/test/java/org/springframework/cli/testutil/TestResourceUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ public abstract class TestResourceUtils {
 		return new ClassPathResource(String.format("%s-%s", clazz.getSimpleName(), resourceSuffix), clazz);
 	}
 
-	public static String resourceContents(Class clazz, String resourceName) {
+	public static String resourceContents(Class<?> clazz, String resourceName) {
 		try {
 			return StreamUtils.copyToString(TestResourceUtils.qualifiedResource(clazz, resourceName).getInputStream(),
 					Charset.defaultCharset());
